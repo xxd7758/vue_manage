dfb1d5b6a884b525bd3ad18570f8eda4314787d4
宇 <2051514266@qq.com>
2022-09-13 00:00:56 +0800

    complete params

--- src/views/goods/Params.vue
+++ src/views/goods/Params.vue
@@ -40,13 +40,35 @@
           <el-table :data="manyTableData" border stripe>
             <el-table-column type="expand">
               <template slot-scope="{ row, $index }">
+                <!-- 循环添加tags标签 -->
                 <el-tag
                   v-if="row.attr_vals != ''"
+                  v-for="(item, index) in row.attr_vals"
+                  class="tag_style"
+                  :key="index"
                   type="primary"
                   size="normal"
                   closable
-                  @close=""
-                  >{{ row.attr_vals }}</el-tag
+                  @close="tagClose(index, row)"
+                  >{{ item }}</el-tag
+                >
+                <!-- 动态切换 按钮 input标签 -->
+                <el-input
+                  class="input_new_tag"
+                  v-if="row.inputVisible"
+                  v-model="row.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(row)"
+                  @blur="handleInputConfirm(row)"
+                >
+                </el-input>
+                <el-button
+                  v-else
+                  class="button-new-tag"
+                  size="small"
+                  @click="showInput(row)"
+                  >+ New Tag</el-button
                 >
               </template>
             </el-table-column>
@@ -88,13 +110,35 @@
           <el-table :data="onlyTableData" border stripe>
             <el-table-column type="expand">
               <template slot-scope="{ row, $index }">
+                <!-- 循环添加tags标签 -->
                 <el-tag
                   v-if="row.attr_vals != ''"
+                  v-for="(item, index) in row.attr_vals"
+                  class="tag_style"
+                  :key="index"
                   type="primary"
                   size="normal"
                   closable
-                  @close=""
-                  >{{ row.attr_vals }}</el-tag
+                  @close="tagClose(index, row)"
+                  >{{ item }}</el-tag
+                >
+                <!-- 动态切换 按钮 input标签 -->
+                <el-input
+                  class="input_new_tag"
+                  v-if="row.inputVisible"
+                  v-model="row.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(row)"
+                  @blur="handleInputConfirm(row)"
+                >
+                </el-input>
+                <el-button
+                  v-else
+                  class="button-new-tag"
+                  size="small"
+                  @click="showInput(row)"
+                  >+ New Tag</el-button
                 >
               </template>
             </el-table-column>
@@ -196,6 +240,8 @@ export default {
       },
       editDialogVisible: false, //编辑对话框
       editDialogForm: { attr_name: "", cat_id: "", attr_id: "", attr_sel: "" }, //保存修改 参数 对象
+      // inputValue: "", //添加新的tags标签 值
+      // inputVisible: false, //控制 新增tags标签的动态切换    -------单独每项添加属性 单独控制
     };
   },
   created() {
@@ -230,11 +276,20 @@ export default {
           params: { sel: this.activeName },
         }
       );
-      // console.log(res);
+      console.log(res);
       if (res.meta.status != 200)
         return this.$message.error("获取参数列表失败");
+      res.data.forEach((item) => {
+        console.log(item);
+        //tips 初始化为空对象 否则 添加第一项时前面多一项空格标签 
+        item.attr_vals = !item.attr_vals ? [] : item.attr_vals.split(" ");
+        console.log(item.attr_vals);
+        item.inputVisible = false; // 每个数据 单独控制
+        item.inputValue = "";
+      });
       // 数据分别存储
       if (this.activeName == "many") {
+        // console.log(res.data);
         this.manyTableData = res.data;
       } else {
         this.onlyTableData = res.data;
@@ -292,6 +347,7 @@ export default {
     editDialogClosed() {
       this.$refs.editDialogRef.resetFields();
     },
+    // 删除 参数
     deleteParmas(row) {
       this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
         confirmButtonText: "确定",
@@ -316,6 +372,53 @@ export default {
           });
         });
     },
+    // 文本框失去焦点 / 回车  转换成button按钮 并添加新tag标签
+    handleInputConfirm(row) {
+      if (row.inputValue.trim().length == "") {
+        row.inputValue = "";
+        row.inputVisible = false;
+        return;
+      }
+      // 本地添加
+      row.attr_vals.push(row.inputValue.trim());
+      row.inputValue = "";
+      row.inputVisible = false;
+      this.saveAttrVals(row);
+    },
+    // 保存 attrvals 参数
+    async saveAttrVals(row) {
+      // console.log(row);
+      // 发请求 保存到数据库中
+      // 分类ID
+      // const cat_id = this.selectedCate[this.selectedCate.length - 1];
+      let { data: res } = await this.$http.put(
+        `categories/${row.cat_id}/attributes/${row.attr_id}`,
+        {
+          attr_name: row.attr_name,
+          attr_sel: row.attr_sel,
+          attr_vals: row.attr_vals.join(" "),
+        }
+      );
+      console.log(res);
+      if (res.meta.status != 200)
+        return this.$message.error("修改参数 tags失败");
+      this.$message.success("修改tags成功");
+    },
+    // 删除 tag
+    tagClose(index, row) {
+      // 删除 截取最后一个
+      console.log(index, row);
+      row.attr_vals.splice(index, 1);
+      this.saveAttrVals(row);
+    },
+    //点击 展示文本框
+    showInput(row) {
+      row.inputVisible = true;
+      // 自动获取焦点
+      this.$nextTick((_) => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
   },
   computed: {
     // 动态 计算 对话框标题
@@ -329,5 +432,11 @@ export default {
 <style scoped>
 .cate_choose {
   margin: 20px;
+}
+.tag_style {
+  margin: 5px;
+}
+.input_new_tag {
+  width: 120px;
 }
 </style>
